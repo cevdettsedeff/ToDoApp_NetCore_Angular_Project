777b4a3d79e7f2480e5dfb1bc176ffda2f660e3c
Cevdet Sedef <cevdettsedeff@gmail.com>
2023-08-18 16:38:24 +0300

    Backend was created.
    A backend was created for the Angular project using the .Net Core API.

---
+++ ToDoApp_API/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp_API.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime Created { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
+
---
+++ ToDoApp_API/Seeds/TodoSeed.cs
@@ -0,0 +1,15 @@
+﻿using ToDoApp_API.Models;
+
+namespace ToDoApp_API.Seeds
+{
+    public class TodoSeed
+    {
+        public static void AddTodosDummy(AppDbContext context)
+        {
+            if (context.Todos!.Any()) return;
+            context.Todos!.Add(new() { Content = "Yapılacak iş 1", Created = DateTime.Now, IsCompleted = false });
+            context.Todos!.Add(new() { Content = "yapılacak iş 2", Created = DateTime.Now, IsCompleted = false });
+            context.SaveChanges();
+        }
+    }
+}
